8de7d6c251a2f9d9f45e6d1cc5e696a8de9eb438
Michael Ndula <53141613+Michaelndula@users.noreply.github.com>
2024-08-20 05:37:52 +0300

    Setup MOH 204A Report (Downloadable Template) (#6)

--- api/src/main/java/org/openmrs/module/ehospitalreports/reporting/library/datasets/Moh204DatasetDefinition.java
+++ api/src/main/java/org/openmrs/module/ehospitalreports/reporting/library/datasets/Moh204DatasetDefinition.java
@@ -1,31 +1,39 @@
 package org.openmrs.module.ehospitalreports.reporting.library.datasets;
 
 import org.openmrs.*;
-import org.openmrs.api.VisitService;
 import org.openmrs.api.context.Context;
+import org.openmrs.module.ehospitalreports.reporting.calculation.WardAddressCalculation;
+import org.openmrs.module.ehospitalreports.reporting.data.converter.CalculationResultConverter;
 import org.openmrs.module.ehospitalreports.reporting.data.converter.EncounterDateConverter;
+import org.openmrs.module.ehospitalreports.reporting.library.data.converter.BooleanToYesNoConverter;
 import org.openmrs.module.ehospitalreports.reporting.library.data.converter.PersonAttributeDataConverter;
+import org.openmrs.module.ehospitalreports.reporting.library.data.definition.CalculationDataDefinition;
+import org.openmrs.module.ehospitalreports.reporting.library.data.definition.RevisitPatientCalculation;
 import org.openmrs.module.ehospitalreports.reporting.utils.constants.reports.shared.SharedReportConstants;
 import org.openmrs.module.reporting.common.SortCriteria;
-import org.openmrs.module.reporting.common.TimeQualifier;
 import org.openmrs.module.reporting.data.DataDefinition;
 import org.openmrs.module.reporting.data.converter.DataConverter;
 import org.openmrs.module.reporting.data.converter.ObjectFormatter;
+import org.openmrs.module.reporting.data.converter.ObsValueConverter;
 import org.openmrs.module.reporting.data.patient.definition.ConvertedPatientDataDefinition;
-import org.openmrs.module.reporting.data.patient.definition.EncountersForPatientDataDefinition;
 import org.openmrs.module.reporting.data.patient.definition.PatientIdDataDefinition;
 import org.openmrs.module.reporting.data.patient.definition.PatientIdentifierDataDefinition;
 import org.openmrs.module.reporting.data.person.definition.*;
+import org.openmrs.module.reporting.data.visit.definition.VisitDataDefinition;
 import org.openmrs.module.reporting.dataset.definition.DataSetDefinition;
 import org.openmrs.module.reporting.dataset.definition.PatientDataSetDefinition;
 import org.openmrs.module.reporting.evaluation.parameter.Parameter;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
-import java.util.List;
 
 @Component
 public class Moh204DatasetDefinition extends eHospitalBaseDataSet {
+	
+	private DataDefinition personWardAddress() {
+		CalculationDataDefinition cd = new CalculationDataDefinition("Ward", new WardAddressCalculation());
+		return cd;
+	}
 	
 	public DataSetDefinition constructMoh204ADatasetDefinition() {
 		PatientDataSetDefinition dsd = new PatientDataSetDefinition();
@@ -49,23 +57,76 @@ public class Moh204DatasetDefinition extends eHospitalBaseDataSet {
 		    SharedReportConstants.PHONE_NUMBER_ATTRIBUTE_TYPE_UUID);
 		
 		dsd.addColumn("id", new PatientIdDataDefinition(), "");
-		dsd.addColumn("Identifier", identifierDef, (String) null);
-		dsd.addColumn("Date", getEncounterDate(), "onOrAfter=${startDate},onOrBefore=${endDate+23h}",
-		    new EncounterDateConverter());
+		dsd.addColumn("Date", CommonDatasetDefinition.getEncounterDate(),
+		    "onOrAfter=${startDate},onOrBefore=${endDate+23h}", new EncounterDateConverter());
+		dsd.addColumn("OPD No", identifierDef, (String) null);
+		dsd.addColumn("Revisit", getRevisit(), (String) null);
 		dsd.addColumn("Name", nameDef, "");
 		dsd.addColumn("Age", new AgeDataDefinition(), "", null);
 		dsd.addColumn("Gender", new GenderDataDefinition(), "", null);
+		dsd.addColumn("Ward", personWardAddress(), "", new CalculationResultConverter());
 		dsd.addColumn("Telephone", new PersonAttributeDataDefinition("Phone Number", phoneNumber), "",
 		    new PersonAttributeDataConverter());
+		dsd.addColumn(
+		    "Weight",
+		    CommonDatasetDefinition.getObservation(Context.getConceptService().getConceptByUuid(
+		        SharedReportConstants.WEIGHT_KG)), "onOrAfter=${startDate},onOrBefore=${endDate+23h}",
+		    new ObsValueConverter());
+		dsd.addColumn(
+		    "Height",
+		    CommonDatasetDefinition.getObservation(Context.getConceptService().getConceptByUuid(
+		        SharedReportConstants.HEIGHT_CM)), "onOrAfter=${startDate},onOrBefore=${endDate+23h}",
+		    new ObsValueConverter());
+		dsd.addColumn("BMI",
+		    CommonDatasetDefinition.getObservation(Context.getConceptService().getConceptByUuid(SharedReportConstants.BMI)),
+		    "onOrAfter=${startDate},onOrBefore=${endDate+23h}", new ObsValueConverter());
+		dsd.addColumn(
+		    "Supplementation",
+		    CommonDatasetDefinition.getObservation(Context.getConceptService().getConceptByUuid(
+		        SharedReportConstants.SUPPLEMENTATION)), "onOrAfter=${startDate},onOrBefore=${endDate+23h}",
+		    new ObsValueConverter());
+		dsd.addColumn(
+		    "Visual Acuity right eye",
+		    CommonDatasetDefinition.getObservation(Context.getConceptService().getConceptByUuid(
+		        SharedReportConstants.VISUAL_ACUITY_RIGHT_EYE)), "onOrAfter=${startDate},onOrBefore=${endDate+23h}",
+		    new ObsValueConverter());
+		dsd.addColumn(
+		    "Visual Acuity left eye",
+		    CommonDatasetDefinition.getObservation(Context.getConceptService().getConceptByUuid(
+		        SharedReportConstants.VISUAL_ACUITY_LEFT_EYE)), "onOrAfter=${startDate},onOrBefore=${endDate+23h}",
+		    new ObsValueConverter());
+		dsd.addColumn(
+		    "Diagnosis",
+		    CommonDatasetDefinition.getObservation(Context.getConceptService().getConceptByUuid(
+		        SharedReportConstants.DIAGNOSIS)), "onOrAfter=${startDate},onOrBefore=${endDate+23h}",
+		    new ObsValueConverter());
+		dsd.addColumn(
+		    "Treatment",
+		    CommonDatasetDefinition.getObservation(Context.getConceptService().getConceptByUuid(
+		        SharedReportConstants.TREATMENT)), "onOrAfter=${startDate},onOrBefore=${endDate+23h}",
+		    new ObsValueConverter());
+		dsd.addColumn(
+		    "Outcome",
+		    CommonDatasetDefinition.getObservation(Context.getConceptService().getConceptByUuid(
+		        SharedReportConstants.OUTCOME)), "onOrAfter=${startDate},onOrBefore=${endDate+23h}", new ObsValueConverter());
+		dsd.addColumn(
+		    "Referred From",
+		    CommonDatasetDefinition.getObservation(Context.getConceptService().getConceptByUuid(
+		        SharedReportConstants.REFFERED_FROM)), "onOrAfter=${startDate},onOrBefore=${endDate+23h}",
+		    new ObsValueConverter());
+		dsd.addColumn(
+		    "Referred To",
+		    CommonDatasetDefinition.getObservation(Context.getConceptService().getConceptByUuid(
+		        SharedReportConstants.REFFERED_TO)), "onOrAfter=${startDate},onOrBefore=${endDate+23h}",
+		    new ObsValueConverter());
 		
 		return dsd;
 	}
 	
-	private DataDefinition getEncounterDate() {
-		EncountersForPatientDataDefinition dsd = new EncountersForPatientDataDefinition();
-		dsd.addParameter(new Parameter("onOrAfter", "After Date", Date.class));
-		dsd.addParameter(new Parameter("onOrBefore", "Before Date", Date.class));
-		dsd.setWhich(TimeQualifier.LAST);
-		return dsd;
+	private DataDefinition getRevisit() {
+		CalculationDataDefinition cd = new CalculationDataDefinition("Revisit", new RevisitPatientCalculation());
+		cd.addParameter(new Parameter("endDate", "End Date", Date.class));
+		return new ConvertedPatientDataDefinition(cd, new BooleanToYesNoConverter());
 	}
+	
 }
--- api/src/main/java/org/openmrs/module/ehospitalreports/reporting/utils/constants/reports/shared/SharedReportConstants.java
+++ api/src/main/java/org/openmrs/module/ehospitalreports/reporting/utils/constants/reports/shared/SharedReportConstants.java
@@ -4,6 +4,8 @@ public class SharedReportConstants {
 	
 	public static final String MOH_204_REGISTER_REPORT_UUID = "26985654-8c25-11ed-8e58-b7227b8d5928";
 	
+	public static final String OPD_REVISIT_UUID = "68391cec-08be-454d-8182-ff4e567de66d";
+	
 	public static final String OPENMRS_ID_IDENTIFIER_TYPE = "8d793bee-c2cc-11de-8d13-0010c6dffd0f";
 	
 	public static final String UNIQUE_OPD_NUMBER = "164180AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
@@ -25,5 +27,27 @@ public class SharedReportConstants {
 	public static final String HIV_STATUS_NEGATIVE_CONCEPT_UUID = "TO DEFINE";
 	
 	public static final String HIV_STATUS_UNKNOWN_CONCEPT_UUID = "TO DEFINE";
+	
+	public static final String WEIGHT_KG = "5089AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
+	
+	public static final String HEIGHT_CM = "5090AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
+	
+	public static final String BMI = "1342AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
+	
+	public static final String SUPPLEMENTATION = "f5a93555-0455-4de9-93c7-92111d8ddb72";
+	
+	public static final String VISUAL_ACUITY_RIGHT_EYE = "108485BBBBBBBBBBBBBBBBBBBBBBBBBBBBBB";
+	
+	public static final String VISUAL_ACUITY_LEFT_EYE = "108483BBBBBBBBBBBBBBBBBBBBBBBBBBBBBB";
+	
+	public static final String DIAGNOSIS = "759bf916-5549-4fe1-a588-a399ba04dfd5";
+	
+	public static final String TREATMENT = "e670dc2a-0c21-4fda-a8df-d9b7b1ca87ec";
+	
+	public static final String OUTCOME = "6990f720-b0f7-405d-bfb6-7a1f058109bb";
+	
+	public static final String REFFERED_FROM = "44335370-b96b-405e-97ad-ea4d4451a2ca";
+	
+	public static final String REFFERED_TO = "a9b68c20-8ace-459a-8e7b-68c9a8487aba";
 	
 }
